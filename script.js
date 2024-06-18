1896e6186d565a5938a9f75d3a1a533349fc2079
MKNTW <maksim1771314@gmail.com>
2024-06-18 13:43:11 +0300

    Update script.js
    Signed-off-by: MKNTW <maksim1771314@gmail.com>

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-// Function to retrieve Ton wallet from local storage
-function getTonWalletFromStorage() {
-    return localStorage.getItem('tonWallet') || '';
+// Function to retrieve ZCOIN from local storage
+function getCoinsFromStorage() {
+    return parseFloat(localStorage.getItem('coins')) || 0;
 }
 
-// Function to save Ton wallet to local storage
-function saveTonWalletToStorage(wallet) {
-    localStorage.setItem('tonWallet', wallet);
+// Function to save ZCOIN to local storage
+function saveCoinsToStorage(coins) {
+    localStorage.setItem('coins', coins.toString());
 }
 
-// Display Ton wallet on page load
-window.onload = function() {
-    const storedWallet = getTonWalletFromStorage();
-    if (storedWallet) {
-        displayTonWallet(storedWallet);
+// Function to save wallet name to local storage
+function saveWalletName(name) {
+    localStorage.setItem('walletName', name);
+}
+
+// Function to get wallet name from local storage
+function getWalletName() {
+    return localStorage.getItem('walletName') || '';
+}
+
+let coins = getCoinsFromStorage();
+document.getElementById('coins').innerText = `ZCOIN: ${coins.toFixed(5)}`;
+
+// Update wallet display if already stored
+document.getElementById('walletDisplay').innerText = `Кошелек: ${getWalletName()}`;
+
+document.getElementById('tapArea').addEventListener('click', function() {
+    coins += 0.00001;
+    document.getElementById('coins').innerText = `ZCOIN: ${coins.toFixed(5)}`;
+    saveCoinsToStorage(coins);
+
+    // Button press animation
+    this.style.transform = 'scale(0.95)';
+    setTimeout(() => {
+        this.style.transform = 'scale(1)';
+    }, 50);
+});
+
+// Save wallet name on button click
+document.getElementById('confirmWalletBtn').addEventListener('click', function() {
+    const walletName = document.getElementById('walletNameInput').value.trim();
+    if (walletName !== '') {
+        saveWalletName(walletName);
+        document.getElementById('walletDisplay').innerText = `Кошелек: ${walletName}`;
+        document.getElementById('walletNameInput').value = ''; // Clear input field
     }
-};
-
-// Function to display Ton wallet
-function displayTonWallet(wallet) {
-    const walletDisplay = document.getElementById('walletDisplay');
-    walletDisplay.innerText = `Кошелек: ${wallet}`;
-}
-
-// Event listener for input field
-const walletInputField = document.getElementById('walletInputField');
-walletInputField.addEventListener('change', function() {
-    const wallet = this.value.trim();
-    saveTonWalletToStorage(wallet);
-    displayTonWallet(wallet);
-    this.value = ''; // Clear input field after saving
 });
