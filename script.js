912e943f764dc59dbaebc792b4e8871a1eb7cfb1
MKNTW <maksim1771314@gmail.com>
2025-02-16 21:21:51 +0300

    Update script.js
    Signed-off-by: MKNTW <maksim1771314@gmail.com>

--- script.js
+++ script.js
@@ -1107,7 +1107,7 @@ async function loadBalanceAndExchangeRate() {
   }
 
   try {
-    const rateResp = await fetch(`${API_URL}/exchangeRates?limit=200`, {
+    const rateResp = await fetch(`${API_URL}/exchangeRates?limit=25`, {
       credentials: "include",
     });
     const rateData = await rateResp.json();
