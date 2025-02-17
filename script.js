f9420b46e5c7210404034d0ff0f8cea48a26d7cb
MKNTW <maksim1771314@gmail.com>
2025-02-17 09:46:24 +0300

    Update script.js
    Signed-off-by: MKNTW <maksim1771314@gmail.com>

--- script.js
+++ script.js
@@ -1742,6 +1742,7 @@ function createMerchantQR(amount, purpose) {
         box-shadow:0 2px 5px rgba(0,0,0,0.1);
         display:flex;
         flex-direction:column;
+        margin-top:30px;
         align-items:center;
       ">
 
